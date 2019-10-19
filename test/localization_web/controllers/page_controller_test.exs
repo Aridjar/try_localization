bac287b5ecf236d119e8ff2fb566afb41805c77e
Aridjar <cedric@paumard.net>
2019-10-20 01:43:17 +0200

    test created and fail as expected

--- test/localization_web/controllers/page_controller_test.exs
+++ test/localization_web/controllers/page_controller_test.exs
@@ -5,4 +5,9 @@ defmodule LocalizationWeb.PageControllerTest do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
+
+  test "GET /v1/get_job_offers", %{conn: conn} do
+    conn = get(conn, "/v1/get_job_offers?latitude=48.8659387&longitude=2.34532&radius=10)")
+    assert json_response(conn, 200) =~ ""
+  end
 end
